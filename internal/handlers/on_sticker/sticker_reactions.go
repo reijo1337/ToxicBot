117225f9c180b2bda4dc5fcce86cee6e726dbe39
Danila Balyakin <dvbalyakin@avito.ru>
2022-11-06 20:42:49 +0300

    fix handler

--- internal/handlers/on_sticker/sticker_reactions.go
+++ internal/handlers/on_sticker/sticker_reactions.go
@@ -13,19 +13,21 @@ import (
 )
 
 type StickerReactions struct {
-	cfg      config
-	stickers []string
-	muStk    sync.RWMutex
-	r        *rand.Rand
-	storage  storage.Manager
-	logger   *logrus.Logger
+	cfg               config
+	stickers          []string
+	stickersFromPacks []string
+	muStk             sync.RWMutex
+	r                 *rand.Rand
+	storage           storage.Manager
+	logger            *logrus.Logger
 }
 
 func New(ctx context.Context, stor storage.Manager, logger *logrus.Logger, stickersFromPacks []string) (*StickerReactions, error) {
 	out := StickerReactions{
-		storage: stor,
-		logger:  logger,
-		r:       rand.New(rand.NewSource(time.Now().UnixNano())),
+		storage:           stor,
+		logger:            logger,
+		stickersFromPacks: stickersFromPacks,
+		r:                 rand.New(rand.NewSource(time.Now().UnixNano())),
 	}
 
 	if err := out.parseConfig(); err != nil {
@@ -46,9 +48,13 @@ func (sr *StickerReactions) Handle(ctx telebot.Context) error {
 	}
 
 	sr.muStk.RLock()
-	defer sr.muStk.RUnlock()
-	randomIndex := sr.r.Intn(len(sr.stickers))
-	sticker := sr.stickers[randomIndex]
+	s := make([]string, 0, len(sr.stickersFromPacks)+len(sr.stickers))
+	s = append(s, sr.stickers...)
+	s = append(s, sr.stickersFromPacks...)
+	sr.muStk.RUnlock()
+
+	randomIndex := sr.r.Intn(len(s))
+	sticker := s[randomIndex]
 
 	return ctx.Reply(&telebot.Sticker{File: telebot.File{FileID: sticker}})
 }
